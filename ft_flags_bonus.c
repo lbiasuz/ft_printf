83351e2bb70d97d4b24d3aa27bc56b6ce5e2c9d8
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-07-02 12:41:45 -0300

    Adicionadas funções de flags secundárias

--- ft_flags_bonus.c
+++ ft_flags_bonus.c
@@ -6,46 +6,13 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 21:38:45 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/07/01 00:01:17 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/07/02 12:20:19 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-
-t_flags	*get_flags(char *str)
-{
-	t_flags	*flags;
-
-	flags = (t_flags *) malloc(sizeof(t_flags));
-	while(*str && !ft_strchr("csdiuxX%", *str))
-	{
-		if (*str == ' ')
-			flags->blank_space = 1;
-		if (*str == '+')
-			flags->sign = 1;
-		if (*str == '-')
-			flags->left_pad = 1;
-		if (*str == '#')
-			flags->hashtag = 1;
-		if (*str == '0' && ft_isdigit(*(str++)))
-			flags->zeroes = 1;
-		if (ft_isdigit(*str))
-		{
-			flags->min_size = ft_atoi(str);
-			str += long_long_size(flags->min_size, 10) - 1;
-		}
-		if (*str == '.' && ft_isdigit(*(str + 1)))
-		{
-			flags->precision = ft_atoi((str + 1));
-			str += long_long_size(flags->precision, 10) - 1;
-		}
-		str++;
-	}
-	return (flags);
-}
-
-char *ft_precision_flag(char *str, char *flags)
+char	*ft_precision_flag(char *str, char *flags)
 {
 	unsigned int	precision;
 	unsigned int	str_len;
@@ -59,12 +26,51 @@ char *ft_precision_flag(char *str, char *flags)
 		if (precision <= str_len)
 			precision = str_len;
 		else
-			precision -= str_len;
-		temp = (char *) malloc(sizeof(char) * (precision));
+			precision -= (str_len);
+		temp = (char *) ft_calloc(precision + 1, sizeof(char));
 		ft_memset(temp, '0', precision);
-		temp[precision - 1] = 0;
 		value = ft_strjoin(temp, str);
 		free(temp);
+	}
+	else
+		value = str;
+	return (value);
+}
+
+char *ft_blank_flag(char *str, char *flags)
+{
+	char	*value;
+
+	if (ft_strchr(flags, ' '))
+		value = ft_strjoin(" ", str);
+	else
+		value = str;
+	return (value);
+}
+
+char *ft_sign_flag(char *str, char *flags)
+{
+	char			*value;
+	unsigned int	is_digit;
+
+	is_digit = ft_strchr(flags, 'd') || ft_strchr(flags, 'i');
+	if (ft_strchr(flags, '+') && (is_digit) && ft_atoi(str) >= 0)
+		value = ft_strjoin("+", str);
+	else
+		value = str;
+	return (value);
+}
+
+char *ft_hashtag_flag(char *str, char *flags)
+{
+	char			*value;
+	unsigned int	is_hex;
+	is_hex = ft_strchr(flags, 'x') || ft_strchr(flags, 'X');
+	if (ft_strchr(flags, '#') && ft_atoi(str) != 0)
+	{
+		value = ft_strjoin("0x", str);
+		if (ft_strchr(flags, 'X'))
+			value[1] = 'X';
 	}
 	else
 		value = str;
